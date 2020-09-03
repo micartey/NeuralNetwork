e6f6280d371230951bd10810c04bc0861a0bbace
Clientastisch <daniel.lingemann@aol.com>
2020-09-03 17:49:33 +0200

    Add activation function

--- src/main/java/me/clientastisch/network/neural/layer/Neuron.java
+++ src/main/java/me/clientastisch/network/neural/layer/Neuron.java
@@ -2,6 +2,7 @@ package me.clientastisch.network.neural.layer;
 
 import lombok.Getter;
 import lombok.Setter;
+import me.clientastisch.network.neural.functions.FunctionType;
 
 import java.io.Serializable;
 
@@ -11,13 +12,13 @@ public class Neuron implements Serializable, Cloneable {
     @Setter private double[] weights;
     @Setter private double bias;
 
-    @Setter private double error;
-    @Setter private double output;
-    @Setter private double derivatOutput;
-
+    @Setter private double error, output, derivatOutput;
     private int index;
 
-    public Neuron(int index) {
+    private final FunctionType function;
+
+    public Neuron(FunctionType function, int index) {
+        this.function = function;
         this.index = index;
     }
 }
